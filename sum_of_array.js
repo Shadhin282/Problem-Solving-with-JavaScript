0c8a26e8bb4b04a78c7d4d9ba27bd210d3152141
Md. Mahmodul Hassen Khan Shadin <shadhinkhan28@gmail.com>
2026-06-09 08:08:01 +0000

    sum of Array

---
+++ sum_of_array.js
@@ -0,0 +1,18 @@
+// Problem 11: Find the Sum of an Array  [Easy]
+// Description: Write a function sumArray(arr) that returns the sum of all numbers in an array.
+// Example:
+// Input: [1, 2, 3, 4, 5]  → Output: 15
+// Hint: Use reduce() or a for loop.
+
+function sumArray(arr) {
+    if (!Array.isArray(arr)) {
+        return arr + ' is not array.'
+    }
+    const total = arr.reduce((sum, num) => sum + num, 0)
+
+    return total;
+}
+
+let num = [1, 2, 3, 4, 5];
+
+console.log(sumArray(num));
